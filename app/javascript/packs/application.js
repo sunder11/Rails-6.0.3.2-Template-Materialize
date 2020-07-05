946bde546282d3ca45fed5e6186984b08487bdc2
Stephen K Underwood <steve@underwoodlaw.net>
2020-07-05 00:45:38 -0400

    added materialize-css and jquiry

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
+import 'materialize-css/dist/js/materialize'
+import "../stylesheets/application"
 
+document.addEventListener("turbolinks:load" , () => {
+    $('.sidenav').sidenav()
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
