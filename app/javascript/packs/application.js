3f3f2c5e6f511d68d618065733e958a6e8a97efc
Stephen K Underwood <steve@underwoodlaw.net>
2020-07-05 21:39:45 -0400

    added header partial and shim for ie9

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "../stylesheets/application"
 
 document.addEventListener("turbolinks:load" , () => {
     $('.sidenav').sidenav()
+    $(".dropdown-trigger").dropdown()
 })
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
